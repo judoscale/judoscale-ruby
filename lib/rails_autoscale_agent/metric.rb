e43b9533081e7988e90d9a4ced5f0beb566ef552
Adam McCrea <adam@adamlogic.com>
2016-12-05 09:57:51 -0500

    Treat all times as UTC

--- lib/rails_autoscale_agent/metric.rb
+++ lib/rails_autoscale_agent/metric.rb
@@ -1,7 +1,7 @@
 module RailsAutoscaleAgent
   class Metric < Struct.new(:type, :time, :value)
     def initialize(type, time, value)
-      super type, time, value.to_i
+      super type, time.utc, value.to_i
     end
   end
 end
