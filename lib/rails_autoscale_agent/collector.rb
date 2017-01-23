cc7c6abe7a8cd3df2eeb4b3382d8d5d6342781ce
Adam McCrea <adam@adamlogic.com>
2017-01-22 21:34:27 -0500

    Handle negative queue times

--- lib/rails_autoscale_agent/collector.rb
+++ lib/rails_autoscale_agent/collector.rb
@@ -8,6 +8,7 @@ module RailsAutoscaleAgent
           puts "[rails-autoscale] [Collector] request queued for more than 10 minutes... skipping collection"
         else
           queue_time_millis = (Time.now - request.entered_queue_at) * 1000
+          queue_time_millis = 0 if queue_time_millis < 0
           store.push(queue_time_millis)
         end
       else
