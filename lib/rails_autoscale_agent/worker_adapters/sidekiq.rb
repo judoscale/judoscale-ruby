331012500366e500baa3c3cca6dcb31219a776e4
Adam McCrea <adam@adamlogic.com>
2020-03-20 09:32:49 -0400

    Add hack to avoid killing long-running jobs

--- lib/rails_autoscale_agent/worker_adapters/sidekiq.rb
+++ lib/rails_autoscale_agent/worker_adapters/sidekiq.rb
@@ -16,9 +16,19 @@ module WorkerAdapters
 
     def collect!(store)
       log_msg = String.new('Sidekiq latency ')
+      workers_by_queue = ::Sidekiq::Workers.new.group_by do |process_id, thread_id, work|
+        work.dig 'payload', 'queue'
+      end
 
       ::Sidekiq::Queue.all.each do |queue|
+        workers = Array(workers_by_queue[queue.name])
         latency_ms = (queue.latency * 1000).ceil
+
+        # If there are active running jobs on this queue, ensure the latency is
+        # reported as at least 200ms. This is a hack to avoid downscaling and
+        # killing long-running jobs.
+        latency_ms = 200 if latency_ms < 200 && workers.any?
+
         store.push latency_ms, Time.now, queue.name
         log_msg << "#{queue.name}=#{latency_ms} "
       end
