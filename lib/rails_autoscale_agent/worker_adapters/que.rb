438f16242a1c79b9a6f1893ca428661846a18d3f
Adam McCrea <adam@adamlogic.com>
2020-03-31 11:33:55 -0400

    Fix comment

--- lib/rails_autoscale_agent/worker_adapters/que.rb
+++ lib/rails_autoscale_agent/worker_adapters/que.rb
@@ -26,7 +26,7 @@ module WorkerAdapters
       log_msg = String.new('Que latency ')
       t = Time.now
 
-      # Ignore failed jobs (the retry run_at is a future time, not reflecting latency)
+      # Ignore failed jobs (they skew latency measurement due to the original run_at)
       sql = 'SELECT queue, min(run_at) FROM que_jobs WHERE error_count = 0 GROUP BY queue'
       run_at_by_queue = Hash[ActiveRecord::Base.connection.select_rows(sql)]
       self.class.queues |= run_at_by_queue.keys
