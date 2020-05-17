f292ac25ce1ab074ad219b31511e09054e514b16
Adam McCrea <adam@adamlogic.com>
2020-05-16 21:50:49 -0400

    Add experimental Resque support

---
+++ lib/rails_autoscale_agent/worker_adapters/resque.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_autoscale_agent/logger'
+
+module RailsAutoscaleAgent
+  module WorkerAdapters
+    class Resque
+      include RailsAutoscaleAgent::Logger
+      include Singleton
+
+      def enabled?
+        require 'resque'
+        true
+      rescue LoadError
+        false
+      end
+
+      def collect!(store)
+        log_msg = String.new
+
+        ::Resque.queues.each do |queue|
+          size = ::Resque.size(queue)
+          store.push size, Time.now, queue
+          log_msg << "resque.#{queue}=#{size} "
+        end
+
+        logger.debug log_msg
+      end
+    end
+  end
+end
