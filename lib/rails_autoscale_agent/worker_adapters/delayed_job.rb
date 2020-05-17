0f6a426654159a6dc2bc1b5c621283bc811e993b
Adam McCrea <adam@adamlogic.com>
2020-05-16 21:50:29 -0400

    Namespace WorkerAdapters within RailsAutoscaleAgent module

--- lib/rails_autoscale_agent/worker_adapters/delayed_job.rb
+++ lib/rails_autoscale_agent/worker_adapters/delayed_job.rb
@@ -2,61 +2,63 @@
 
 require 'rails_autoscale_agent/logger'
 
-module WorkerAdapters
-  class DelayedJob
-    include RailsAutoscaleAgent::Logger
-    include Singleton
+module RailsAutoscaleAgent
+  module WorkerAdapters
+    class DelayedJob
+      include RailsAutoscaleAgent::Logger
+      include Singleton
 
-    class << self
-      attr_accessor :queues
-    end
-
-    def initialize
-      # Track the known queues so we can continue reporting on queues that don't
-      # currently have enqueued jobs.
-      self.class.queues = Set.new
-
-      install if enabled?
-    end
-
-    def enabled?
-      defined? ::Delayed
-    end
-
-    def collect!(store)
-      log_msg = String.new('DelayedJob latency ')
-      t = Time.now
-
-      sql = 'SELECT queue, min(run_at) FROM delayed_jobs GROUP BY queue'
-      run_at_by_queue = Hash[ActiveRecord::Base.connection.select_rows(sql)]
-      queues = self.class.queues | run_at_by_queue.keys
-
-      queues.each do |queue|
-        run_at = run_at_by_queue[queue]
-        run_at = Time.parse(run_at) if run_at.is_a?(String)
-        latency_ms = run_at ? ((t - run_at)*1000).ceil : 0
-        store.push latency_ms, t, queue
-        log_msg << "#{queue}=#{latency_ms} "
+      class << self
+        attr_accessor :queues
       end
 
-      logger.debug log_msg
-    end
+      def initialize
+        # Track the known queues so we can continue reporting on queues that don't
+        # currently have enqueued jobs.
+        self.class.queues = Set.new
 
-    private
+        install if enabled?
+      end
 
-    def install
-      plugin = Class.new(Delayed::Plugin) do
-        require 'delayed_job'
+      def enabled?
+        defined? ::Delayed
+      end
 
-        callbacks do |lifecycle|
-          lifecycle.before(:enqueue) do |job, &block|
-            queue = job.queue || 'default'
-            WorkerAdapters::DelayedJob.queues.add queue
+      def collect!(store)
+        log_msg = String.new('DelayedJob latency ')
+        t = Time.now
+
+        sql = 'SELECT queue, min(run_at) FROM delayed_jobs GROUP BY queue'
+        run_at_by_queue = Hash[ActiveRecord::Base.connection.select_rows(sql)]
+        queues = self.class.queues | run_at_by_queue.keys
+
+        queues.each do |queue|
+          run_at = run_at_by_queue[queue]
+          run_at = Time.parse(run_at) if run_at.is_a?(String)
+          latency_ms = run_at ? ((t - run_at)*1000).ceil : 0
+          store.push latency_ms, t, queue
+          log_msg << "#{queue}=#{latency_ms} "
+        end
+
+        logger.debug log_msg
+      end
+
+      private
+
+      def install
+        plugin = Class.new(Delayed::Plugin) do
+          require 'delayed_job'
+
+          callbacks do |lifecycle|
+            lifecycle.before(:enqueue) do |job, &block|
+              queue = job.queue || 'default'
+              WorkerAdapters::DelayedJob.queues.add queue
+            end
           end
         end
-      end
 
-      Delayed::Worker.plugins << plugin
+        Delayed::Worker.plugins << plugin
+      end
     end
   end
 end
--- lib/rails_autoscale_agent/worker_adapters/sidekiq.rb
+++ lib/rails_autoscale_agent/worker_adapters/sidekiq.rb
@@ -2,28 +2,30 @@
 
 require 'rails_autoscale_agent/logger'
 
-module WorkerAdapters
-  class Sidekiq
-    include RailsAutoscaleAgent::Logger
-    include Singleton
+module RailsAutoscaleAgent
+  module WorkerAdapters
+    class Sidekiq
+      include RailsAutoscaleAgent::Logger
+      include Singleton
 
-    def enabled?
-      require 'sidekiq/api'
-      true
-    rescue LoadError
-      false
-    end
-
-    def collect!(store)
-      log_msg = String.new('Sidekiq latency ')
-
-      ::Sidekiq::Queue.all.each do |queue|
-        latency_ms = (queue.latency * 1000).ceil
-        store.push latency_ms, Time.now, queue.name
-        log_msg << "#{queue.name}=#{latency_ms} "
+      def enabled?
+        require 'sidekiq/api'
+        true
+      rescue LoadError
+        false
       end
 
-      logger.debug log_msg
+      def collect!(store)
+        log_msg = String.new
+
+        ::Sidekiq::Queue.all.each do |queue|
+          latency_ms = (queue.latency * 1000).ceil
+          store.push latency_ms, Time.now, queue.name
+          log_msg << "sidekiq.#{queue.name}=#{latency_ms} "
+        end
+
+        logger.debug log_msg
+      end
     end
   end
 end
