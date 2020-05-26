fa72fc20a08f2d6047f2c52d7223898570b74365
Adam McCrea <adam@adamlogic.com>
2020-05-26 17:32:12 -0400

    Ignore failed Delayed Jobs

--- lib/rails_autoscale_agent/worker_adapters/delayed_job.rb
+++ lib/rails_autoscale_agent/worker_adapters/delayed_job.rb
@@ -27,7 +27,8 @@ module WorkerAdapters
       log_msg = String.new('DelayedJob latency ')
       t = Time.now
 
-      sql = 'SELECT queue, min(run_at) FROM delayed_jobs GROUP BY queue'
+      # Ignore failed jobs (they skew latency measurement due to the original run_at)
+      sql = 'SELECT queue, min(run_at) FROM delayed_jobs WHERE attempts = 0 GROUP BY queue'
       run_at_by_queue = Hash[ActiveRecord::Base.connection.select_rows(sql)]
       queues = self.class.queues | run_at_by_queue.keys
 
