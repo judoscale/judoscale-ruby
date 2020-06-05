a8498afb819b7cb03e45040e401767724376543e
Adam McCrea <adam@adamlogic.com>
2020-06-05 10:42:42 -0400

    Ignore time spent waiting on request bodies
    Large request paylods and slow client connections can skew queue times, so this subtracts it from the queue time measurement.

--- lib/rails_autoscale_agent/request.rb
+++ lib/rails_autoscale_agent/request.rb
@@ -4,13 +4,9 @@ module RailsAutoscaleAgent
   class Request
     include Logger
 
-    attr_reader :id, :entered_queue_at, :path, :method, :size
-
     def initialize(env, config)
       @config = config
       @id = env['HTTP_X_REQUEST_ID']
-      @path = env['PATH_INFO']
-      @method = env['REQUEST_METHOD'].downcase
       @size = env['rack.input'].respond_to?(:size) ? env['rack.input'].size : 0
 
       @entered_queue_at = if unix_millis = env['HTTP_X_REQUEST_START']
@@ -19,6 +15,8 @@ module RailsAutoscaleAgent
         # In dev mode, fake a queue time of 0-1000ms
         Time.now - rand
       end
+
+      @request_body_wait = env['puma.request_body_wait'].to_i
     end
 
     def ignore?
@@ -26,12 +24,14 @@ module RailsAutoscaleAgent
     end
 
     def queue_time
-      if entered_queue_at
-        queue_time = ((Time.now - entered_queue_at) * 1000).to_i
+      if @entered_queue_at
+        queue_time = ((Time.now - @entered_queue_at) * 1000).to_i
         queue_time = 0 if queue_time < 0
-        logger.debug "Collected queue_time=#{queue_time}ms request_id=#{id} request_size=#{size}"
+        logger.debug "Collected queue_time=#{queue_time}ms request_id=#{@id} request_size=#{@size}"
 
-        queue_time
+        # Subtract the time Puma spent waiting on the request body. It's irrelevant to capacity-related queue time.
+        # Without this, slow clients and large request payloads will skew queue time.
+        queue_time - @request_body_wait
       end
     end
   end
