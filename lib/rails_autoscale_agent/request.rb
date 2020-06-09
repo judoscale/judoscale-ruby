9f26242fa8e0bbaedcecfe6676a88d3b8a70c23c
Adam McCrea <adam@adamlogic.com>
2020-06-09 18:55:04 -0400

    Adjust queue time for Puma's request body wait (#25)
    Large request paylods and slow client connections can skew queue times, so this subtracts it from the queue time measurement.

--- lib/rails_autoscale_agent/request.rb
+++ lib/rails_autoscale_agent/request.rb
@@ -4,20 +4,17 @@ module RailsAutoscaleAgent
   class Request
     include Logger
 
-    attr_reader :id, :entered_queue_at, :path, :method, :size
-
     def initialize(env, config)
       @config = config
       @id = env['HTTP_X_REQUEST_ID']
-      @path = env['PATH_INFO']
-      @method = env['REQUEST_METHOD'].downcase
       @size = env['rack.input'].respond_to?(:size) ? env['rack.input'].size : 0
+      @request_body_wait = env['puma.request_body_wait'].to_i
 
       @entered_queue_at = if unix_millis = env['HTTP_X_REQUEST_START']
         Time.at(unix_millis.to_f / 1000)
       elsif config.dev_mode?
         # In dev mode, fake a queue time of 0-1000ms
-        Time.now - rand
+        Time.now - rand + @request_body_wait
       end
     end
 
@@ -26,12 +23,17 @@ module RailsAutoscaleAgent
     end
 
     def queue_time
-      if entered_queue_at
-        queue_time = ((Time.now - entered_queue_at) * 1000).to_i
-        queue_time = 0 if queue_time < 0
-        logger.debug "Collected queue_time=#{queue_time}ms request_id=#{id} request_size=#{size}"
+      if @entered_queue_at
+        queue_time = ((Time.now - @entered_queue_at) * 1000).to_i
 
-        queue_time
+        # Subtract the time Puma spent waiting on the request body. It's irrelevant to capacity-related queue time.
+        # Without this, slow clients and large request payloads will skew queue time.
+        queue_time -= @request_body_wait
+
+        logger.debug "Request queue_time=#{queue_time}ms body_wait=#{@request_body_wait}ms request_id=#{@id} size=#{@size}"
+
+        # Safeguard against negative queue times (should not happen in practice)
+        queue_time > 0 ? queue_time : 0
       end
     end
   end
