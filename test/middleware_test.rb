730a75cd56aef2648300fbddc15afeefc08a9466
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2022-01-13 15:01:33 -0300

    Improve exception message in case of test failure
    Seeing "never called" in a test failure / exception could be confusing.
    
    Co-authored-by: Adam McCrea <adam@adamlogic.com>

--- test/middleware_test.rb
+++ test/middleware_test.rb
@@ -95,7 +95,7 @@ module Judoscale
         end
 
         it "does not start the reporter" do
-          Reporter.instance.stub(:register!, -> { raise "NEVER CALLED" }) do
+          Reporter.instance.stub(:register!, -> { raise "SHOULD NOT BE CALLED" }) do
             middleware.call(env)
           end
         end
