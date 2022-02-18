1ccc3934ac7d0324a312200e5186e9427aff9b62
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2022-02-18 18:15:22 -0300

    Introduce a `flush` method to the metrics store
    This will allow the web metrics to be "flushed" at one point in time
    when being collected for reporting, without being involved directly with
    a "Report" object.
    
    For now I'm keeping the `pop_report` method around since it's still
    used, but I'm going to look into a way to refactor it next. I did move
    away from using it in the Middleware tests as they don't really need to
    interact with a "report", they just need to ensure metrics were
    collected, so they can use the new "flush" mechanism now, simplyfing
    and decoupling things there a bit.
    
    Also cover the metrics store with some basic unit tests.

--- lib/judoscale/metrics_store.rb
+++ lib/judoscale/metrics_store.rb
@@ -8,29 +8,38 @@ module Judoscale
   class MetricsStore
     include Singleton
 
-    attr_reader :metrics
+    attr_reader :metrics, :flushed_at
 
     def initialize
       @metrics = []
-      @last_pop = Time.now
+      @flushed_at = Time.now
     end
 
     def push(identifier, value, time = Time.now, queue_name = nil)
       # If it's been two minutes since clearing out the store, stop collecting metrics.
       # There could be an issue with the reporter, and continuing to collect will consume linear memory.
-      return if @last_pop && @last_pop < Time.now - 120
+      return if @flushed_at && @flushed_at < Time.now - 120
 
       @metrics << Metric.new(identifier, time, value, queue_name)
     end
 
-    def pop_report
-      @last_pop = Time.now
-      report = Report.new
+    def flush
+      @flushed_at = Time.now
+      flushed_metrics = []
 
       while (metric = @metrics.shift)
-        report.metrics << metric
+        flushed_metrics << metric
       end
 
+      flushed_metrics
+    end
+
+    # TODO: This is going to be left untouched temporarily while we refactor how the reporting happens.
+    def pop_report
+      flushed_metrics = flush
+
+      report = Report.new
+      report.metrics.concat(flushed_metrics)
       report
     end
 
