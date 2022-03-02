4d5e09704a65a36b897be8047e65b22206eebfef
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2022-03-01 21:04:49 -0300

    Create judoscale-sidekiq skeleton

---
+++ judoscale-sidekiq/lib/judoscale/sidekiq.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+require "judoscale-ruby"
+require "judoscale/sidekiq/version"
---
+++ judoscale-sidekiq/lib/judoscale/sidekiq/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Judoscale
+  module Sidekiq
+    VERSION = "0.1.0.alpha"
+  end
+end
