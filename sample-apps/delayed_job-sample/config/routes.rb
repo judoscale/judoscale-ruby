f7c31da3e00456408269c62238501ad2f22595ef
Adam McCrea <adam@adamlogic.com>
2022-08-17 11:38:44 -0400

    Add DJ web to sample app

--- sample-apps/delayed_job-sample/config/routes.rb
+++ sample-apps/delayed_job-sample/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resource :jobs, only: [:index, :create]
+  match "/delayed_job" => DelayedJobWeb, :anchor => false, :via => [:get, :post]
   root "jobs#index"
 end
