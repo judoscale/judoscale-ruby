1ec7f0ed6f4dc402331a59c5caa69ee939bcde7a
Adam McCrea <adam@adamlogic.com>
2023-01-18 07:27:15 -0500

    Rename variables

--- judoscale-good_job/lib/judoscale/good_job/metrics_collector.rb
+++ judoscale-good_job/lib/judoscale/good_job/metrics_collector.rb
@@ -13,14 +13,14 @@ module Judoscale
 
       def collect
         metrics = []
-        t = Time.now.utc
+        time = Time.now.utc
 
         # TODO: silence query logs for this
-        at = ::GoodJob::Job.arel_table
+        t = ::GoodJob::Job.arel_table
         run_at_by_queue = ::GoodJob::JobsFilter.new(state: "queued").filtered_query.order(
-          :queue_name, at.coalesce(at[:scheduled_at], at[:created_at])
+          :queue_name, t.coalesce(t[:scheduled_at], t[:created_at])
         ).pluck(
-          Arel.sql("distinct on (queue_name) queue_name, #{at.coalesce(at[:scheduled_at], at[:created_at]).to_sql}")
+          Arel.sql("distinct on (queue_name) queue_name, #{t.coalesce(t[:scheduled_at], t[:created_at]).to_sql}")
         ).to_h
 
         self.queues |= run_at_by_queue.keys
@@ -34,10 +34,10 @@ module Judoscale
           run_at = run_at_by_queue[queue]
           # DateTime.parse assumes a UTC string
           run_at = DateTime.parse(run_at) if run_at.is_a?(String)
-          latency_ms = run_at ? ((t - run_at) * 1000).ceil : 0
+          latency_ms = run_at ? ((time - run_at) * 1000).ceil : 0
           latency_ms = 0 if latency_ms < 0
 
-          metrics.push Metric.new(:qt, latency_ms, t, queue)
+          metrics.push Metric.new(:qt, latency_ms, time, queue)
 
           # if track_busy_jobs?
           #   busy_count = busy_count_by_queue[queue] || 0
