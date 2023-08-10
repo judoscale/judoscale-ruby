dd90d5cb847a4d9e7a145800389d3210f195dfa8
Adam McCrea <adam@adamlogic.com>
2023-08-10 11:27:57 -0400

    Properly handle storing GoodJob data in a non-primary database

---
+++ sample-apps/good_job-multi-db-sample/app/controllers/jobs_controller.rb
@@ -0,0 +1,27 @@
+class JobsController < ApplicationController
+  QUEUES = %w[default low high]
+
+  def index
+    @available_queues = QUEUES.dup
+    @queues = []
+    # @queues = Delayed::Backend::ActiveRecord::Job.group(:queue).count
+  end
+
+  def create
+    job_params = params.require(:job).permit(:queue_name, :enqueue_count)
+
+    queue_name = job_params[:queue_name]
+    enqueue_count = job_params[:enqueue_count].to_i
+
+    if QUEUES.include?(queue_name) && enqueue_count.between?(1, 100)
+      1.upto(enqueue_count) {
+        SampleJob.set(queue: queue_name).perform_later
+      }
+
+      flash[:notice] = "#{enqueue_count} #{"job".pluralize(enqueue_count)} enqueued on the #{queue_name.inspect} queue."
+    else
+      flash[:alert] = "Please enter queue name & enqueue count."
+    end
+    redirect_to action: :index
+  end
+end
