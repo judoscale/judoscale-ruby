0da483e610e77cb9710fe789858ef33edaae1d5a
Adam McCrea <adam@adamlogic.com>
2023-12-04 16:03:33 -0500

    Try a larger batch to make it fail for older redis versions

--- judoscale-sidekiq/test/benchmarks/collect_benchmark.rb
+++ judoscale-sidekiq/test/benchmarks/collect_benchmark.rb
@@ -5,9 +5,13 @@ require "minitest/benchmark"
 require "judoscale/sidekiq/metrics_collector"
 
 class CollectBenchmark < Minitest::Benchmark
-  # performance assertions will iterate over `bench_range`
+  BATCH_SIZE = 1_000
+  MAX_RETRIES = 3
+
+  # performance assertions will iterate over `bench_range`.
+  # We'll use it to define the number Sidekiq jobs we enqueue in Redis.
   def self.bench_range
-    bench_exp 10, 100_000 #=> [10, 100, 1000, 10_000, 100_000]
+    bench_exp 10, 1_000_000 #=> [10, 100, 1000, 10000, 100000, 1000000]
   end
 
   def setup
@@ -15,6 +19,7 @@ class CollectBenchmark < Minitest::Benchmark
     Judoscale::Config.instance.reset
 
     @collector = Judoscale::Sidekiq::MetricsCollector.new
+    sidekiq_args = BATCH_SIZE.times.map { [] }
 
     # We need to prepare data for all benchmarks in advance. Each benchmark
     # will target an isolated Redis DB with a different number of jobs.
@@ -22,9 +27,22 @@ class CollectBenchmark < Minitest::Benchmark
       with_isolated_redis(n) do
         Sidekiq.redis(&:flushdb)
 
-        # Enqueue n Sidekiq jobs
-        sidekiq_args = n.times.map { [] }
-        Sidekiq::Client.push_bulk "class" => "DoesNotMatter", "args" => sidekiq_args
+        (n / BATCH_SIZE).times do |i|
+          attempts = 0
+
+          begin
+            Sidekiq::Client.push_bulk "class" => "Foo", "args" => sidekiq_args
+          rescue RedisClient::CannotConnectError => e
+            attempts += 1
+            puts "RESCUED batch #{i}, attempt #{attempts}: #{e.message}"
+
+            # Give the connection a moment to recover
+            sleep(1)
+
+            retry if attempts < MAX_RETRIES
+            raise e
+          end
+        end
       end
     end
   end
@@ -41,7 +59,7 @@ class CollectBenchmark < Minitest::Benchmark
   private
 
   def with_isolated_redis(n, &block)
-    # n is in powers of 10, but we want to use a database number in the range 1-9
+    # n is in powers of 10, but we want to use a database number in the range 0-9
     db_number = Math.log10(n).to_i
 
     if Sidekiq.respond_to?(:default_configuration)
