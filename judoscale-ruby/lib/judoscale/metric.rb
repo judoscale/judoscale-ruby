42d85dc48328a8930ba7797db35155641b70e68e
Adam McCrea <adam@adamlogic.com>
2024-01-23 12:17:53 -0500

    standard style

--- judoscale-ruby/lib/judoscale/metric.rb
+++ judoscale-ruby/lib/judoscale/metric.rb
@@ -5,7 +5,7 @@ module Judoscale
     # No queue_name is assumed to be a web request metric
     # Metrics: qt = queue time (default), qd = queue depth, busy
     def initialize(identifier, value, time, queue_name = nil)
-      super identifier, value.to_i, time.utc, queue_name
+      super(identifier, value.to_i, time.utc, queue_name)
     end
   end
 end
