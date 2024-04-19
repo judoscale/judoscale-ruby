4885b4f43df53641c9ae8f15a674719d7812decd
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2024-04-19 11:17:13 -0300

    Supress migration messages from DJ/GJ
    Make the test output less noisy without those migration messages,
    specially now that GJ is larger with all the tables/indexes.

--- judoscale-delayed_job/test/test_helper.rb
+++ judoscale-delayed_job/test/test_helper.rb
@@ -21,20 +21,22 @@ ActiveRecord::Base.establish_connection(DATABASE_URL)
 ActiveRecord::Schema.define do
   # https://github.com/collectiveidea/delayed_job_active_record/blob/master/lib/generators/delayed_job/templates/migration.rb#L3
   # standard:disable all
-  create_table :delayed_jobs do |table|
-    table.integer :priority, default: 0, null: false # Allows some jobs to jump to the front of the queue
-    table.integer :attempts, default: 0, null: false # Provides for retries, but still fail eventually.
-    table.text :handler,                 null: false # YAML-encoded string of the object that will do work
-    table.text :last_error                           # reason for last failure (See Note below)
-    table.datetime :run_at                           # When to run. Could be Time.zone.now for immediately, or sometime in the future.
-    table.datetime :locked_at                        # Set when a client is working on this object
-    table.datetime :failed_at                        # Set when all retries have failed (actually, by default, the record is deleted instead)
-    table.string :locked_by                          # Who is working on this object (if locked)
-    table.string :queue                              # The name of the queue this job is in
-    table.timestamps null: true
-  end
+  suppress_messages do
+    create_table :delayed_jobs do |table|
+      table.integer :priority, default: 0, null: false # Allows some jobs to jump to the front of the queue
+      table.integer :attempts, default: 0, null: false # Provides for retries, but still fail eventually.
+      table.text :handler,                 null: false # YAML-encoded string of the object that will do work
+      table.text :last_error                           # reason for last failure (See Note below)
+      table.datetime :run_at                           # When to run. Could be Time.zone.now for immediately, or sometime in the future.
+      table.datetime :locked_at                        # Set when a client is working on this object
+      table.datetime :failed_at                        # Set when all retries have failed (actually, by default, the record is deleted instead)
+      table.string :locked_by                          # Who is working on this object (if locked)
+      table.string :queue                              # The name of the queue this job is in
+      table.timestamps null: true
+    end
 
-  add_index :delayed_jobs, [:priority, :run_at], name: "delayed_jobs_priority"
+    add_index :delayed_jobs, [:priority, :run_at], name: "delayed_jobs_priority"
+  end
   # standard:enable all
 end
 
