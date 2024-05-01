9f274def2c47a77f570d1315bbc05915d8cea329
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2024-05-01 13:45:00 -0300

    Implement direct call to SQS to fetch the queue depth attribute value
    Shoryuken provides a private method/API to fetch queue attributes, in
    which we were relying before, but reaching out to private APIs doesn't
    feel really great.
    
    Instead, we're reaching out to SQS directly using a similar
    implementation of the private `Shoryuken::Queue#queue_attributes`, but
    fetching only the attribute we need for reporting.

--- judoscale-shoryuken/lib/judoscale/shoryuken/metrics_collector.rb
+++ judoscale-shoryuken/lib/judoscale/shoryuken/metrics_collector.rb
@@ -6,6 +6,8 @@ require "judoscale/metric"
 module Judoscale
   module Shoryuken
     class MetricsCollector < Judoscale::JobMetricsCollector
+      SQS_QUEUE_DEPTH_ATTRIBUTE = "ApproximateNumberOfMessages"
+
       def self.adapter_config
         Judoscale::Config.instance.shoryuken
       end
@@ -30,8 +32,12 @@ module Judoscale
 
         queues.each do |queue_name|
           queue = queues_by_name[queue_name]
-          # TODO: use public APIs and/or SQS client directly ourselves.
-          depth = queue.send(:queue_attributes).attributes["ApproximateNumberOfMessages"]
+
+          # Reach out to SQS client directly to fetch the queue attribute we need to report.
+          # Shoryuken has a private `queue_attributes` API to fetch all attributes, this call mimics that.
+          sqs_queue_attributes = ::Shoryuken.sqs_client
+            .get_queue_attributes(queue_url: queue.url, attribute_names: [SQS_QUEUE_DEPTH_ATTRIBUTE])
+          depth = sqs_queue_attributes.attributes[SQS_QUEUE_DEPTH_ATTRIBUTE]
 
           metrics.push Metric.new(:qd, depth, Time.now, queue_name)
         end
