fa92b418837acb8b5036e81fa0fc277573e28065
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2025-04-09 10:33:23 -0300

    Calculate queue time and track metric using the same timestamp
    This makes the timestamp a bit more consistent between calculating queue
    time and tracking to the store to report to our backend.

--- judoscale-ruby/lib/judoscale/request_middleware.rb
+++ judoscale-ruby/lib/judoscale/request_middleware.rb
@@ -18,17 +18,18 @@ module Judoscale
       request_metrics = RequestMetrics.new(env)
 
       if request_metrics.track?
-        queue_time = request_metrics.queue_time
+        time = Time.now.utc
+        queue_time = request_metrics.queue_time(time)
         network_time = request_metrics.network_time
         store = MetricsStore.instance
 
         # NOTE: Expose queue time to the app
         env["judoscale.queue_time"] = queue_time
-        store.push :qt, queue_time
+        store.push :qt, queue_time, time
 
         unless network_time.zero?
           env["judoscale.network_time"] = network_time
-          store.push :nt, network_time
+          store.push :nt, network_time, time
         end
 
         logger.debug "Request queue_time=#{queue_time}ms network_time=#{network_time}ms request_id=#{request_metrics.request_id} size=#{request_metrics.size}"
