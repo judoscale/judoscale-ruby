ccd7f9d16528fb0f87e7ac7bf7738b6ebe8fe643
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2025-04-09 10:22:37 -0300

    Cache started at time object
    Started at timestamp doesn't change, but it's called twice when
    calculating queue time (on an initial check to make sure it's available,
    then on the body of the method).
    
    We can cache it to avoid doing extra checks and creating an extra time
    object on each request we track queue time.
    
    It can also be moved to private, since it's not really used by the code
    or tests.

--- judoscale-ruby/lib/judoscale/request_metrics.rb
+++ judoscale-ruby/lib/judoscale/request_metrics.rb
@@ -20,8 +20,27 @@ module Judoscale
       @request_start_header && !ignore_large_request?
     end
 
+    def queue_time(now = Time.now)
+      return if started_at.nil?
+
+      queue_time = ((now - started_at) * 1000).to_i
+
+      # Subtract the time Puma spent waiting on the request body, i.e. the network time. It's irrelevant to
+      # capacity-related queue time. Without this, slow clients and large request payloads will skew queue time.
+      queue_time -= network_time
+
+      # Safeguard against negative queue times (should not happen in practice)
+      (queue_time > 0) ? queue_time : 0
+    end
+
+    private
+
+    def ignore_large_request?
+      @config.ignore_large_requests? && @size > @config.max_request_size_bytes
+    end
+
     def started_at
-      if @request_start_header
+      @started_at ||= if @request_start_header
         # There are several variants of this header. We handle these:
         #   - whole milliseconds (Heroku)
         #   - whole microseconds (???)
@@ -43,25 +62,6 @@ module Judoscale
           Time.at(value)
         end
       end
-    end
-
-    def queue_time(now = Time.now)
-      return if started_at.nil?
-
-      queue_time = ((now - started_at) * 1000).to_i
-
-      # Subtract the time Puma spent waiting on the request body, i.e. the network time. It's irrelevant to
-      # capacity-related queue time. Without this, slow clients and large request payloads will skew queue time.
-      queue_time -= network_time
-
-      # Safeguard against negative queue times (should not happen in practice)
-      (queue_time > 0) ? queue_time : 0
-    end
-
-    private
-
-    def ignore_large_request?
-      @config.ignore_large_requests? && @size > @config.max_request_size_bytes
     end
   end
 end
