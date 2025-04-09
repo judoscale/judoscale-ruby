ce31ec0481aa78843b12fbb6610d93109b1b0b84
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2025-04-09 11:53:11 -0300

    Simplify queue time and remove caching from started at time
    Since we should only call queue time when we're inside a "safe" check
    for whether the request should be tracked, there should be really no
    need for additional checking for potential request started header, we
    should be safe to just go ahead and calculate queue time with the
    values.

--- judoscale-ruby/lib/judoscale/request_metrics.rb
+++ judoscale-ruby/lib/judoscale/request_metrics.rb
@@ -21,10 +21,7 @@ module Judoscale
     end
 
     def queue_time(now = Time.now)
-      return if started_at.nil?
-      now = now.utc
-
-      queue_time = ((now - started_at) * 1000).to_i
+      queue_time = ((now.utc - started_at.utc) * 1000).to_i
 
       # Subtract the time Puma spent waiting on the request body, i.e. the network time. It's irrelevant to
       # capacity-related queue time. Without this, slow clients and large request payloads will skew queue time.
@@ -41,27 +38,24 @@ module Judoscale
     end
 
     def started_at
-      @started_at ||= if @request_start_header
-        # There are several variants of this header. We handle these:
-        #   - whole milliseconds (Heroku)
-        #   - whole microseconds (???)
-        #   - whole nanoseconds (Render)
-        #   - fractional seconds (NGINX)
-        #   - preceeding "t=" (NGINX)
-        value = @request_start_header.gsub(/[^0-9.]/, "").to_f
+      # There are several variants of this header. We handle these:
+      #   - whole milliseconds (Heroku)
+      #   - whole microseconds (???)
+      #   - whole nanoseconds (Render)
+      #   - fractional seconds (NGINX)
+      #   - preceeding "t=" (NGINX)
+      value = @request_start_header.gsub(/[^0-9.]/, "").to_f
 
-        # `value` could be seconds, milliseconds, microseconds or nanoseconds.
-        # We use some arbitrary cutoffs to determine which one it is.
-
-        if value > NANOSECONDS_CUTOFF
-          Time.at(value / 1_000_000_000.0)
-        elsif value > MICROSECONDS_CUTOFF
-          Time.at(value / 1_000_000.0)
-        elsif value > MILLISECONDS_CUTOFF
-          Time.at(value / 1000.0)
-        else
-          Time.at(value)
-        end.utc
+      # `value` could be seconds, milliseconds, microseconds or nanoseconds.
+      # We use some arbitrary cutoffs to determine which one it is.
+      if value > NANOSECONDS_CUTOFF
+        Time.at(value / 1_000_000_000.0)
+      elsif value > MICROSECONDS_CUTOFF
+        Time.at(value / 1_000_000.0)
+      elsif value > MILLISECONDS_CUTOFF
+        Time.at(value / 1000.0)
+      else
+        Time.at(value)
       end
     end
   end
