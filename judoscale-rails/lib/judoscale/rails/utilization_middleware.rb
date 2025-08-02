be40b970e9e86bd193fbf41c5cdc79e48a31d97a
Adam McCrea <adam@adamlogic.com>
2025-08-02 12:41:42 -0400

    Track idle time as an alternative utilization metric
    This adds the "idle_ratio", which is essentially the inverse of utilization. We can calculate it as we go since we always know when the web process is idle. This would avoid the need for sampling, eliminating the extra thread loop and provide a more precise metric for utilization.

--- judoscale-rails/lib/judoscale/rails/utilization_middleware.rb
+++ judoscale-rails/lib/judoscale/rails/utilization_middleware.rb
@@ -7,7 +7,7 @@ require "judoscale/metrics_store"
 module Judoscale
   module Rails
     class UtilizationMiddleware
-      def initialize(app, interval:)
+      def initialize(app, interval: 1)
         @app = app
         @interval = interval
       end
@@ -29,9 +29,14 @@ module Judoscale
       def initialize
         @active_request_counter = Concurrent::AtomicFixnum.new(0)
         @thread_ref = Concurrent::AtomicReference.new(nil)
+        @mutex = Mutex.new
       end
 
-      def start!(interval:)
+      def start!(interval: 1)
+        @mutex.synchronize do
+          reset_idle_report_cycle!
+        end
+
         @thread_ref.update do |current_thread|
           next current_thread if current_thread&.alive?
 
@@ -48,11 +53,48 @@ module Judoscale
       end
 
       def incr
-        @active_request_counter.increment
+        @mutex.synchronize do
+          current_count = @active_request_counter.increment
+
+          if current_count == 1 && @idle_started_at
+            # We were idle and now we're not - add to total idle time
+            @total_idle_time += current_time - @idle_started_at
+            @idle_started_at = nil
+          end
+        end
       end
 
       def decr
-        @active_request_counter.decrement
+        @mutex.synchronize do
+          current_count = @active_request_counter.decrement
+
+          if current_count == 0
+            # We're now idle - start tracking idle time
+            @idle_started_at = current_time
+          end
+        end
+      end
+
+      def idle_ratio(reset: true)
+        @mutex.synchronize do
+          current_time = self.current_time
+
+          total_report_cycle_time = current_time - @report_cycle_started_at
+
+          return 0.0 if total_report_cycle_time <= 0
+
+          # Capture remaining idle time
+          if @idle_started_at
+            @total_idle_time += current_time - @idle_started_at
+            @idle_started_at = current_time
+          end
+
+          idle_ratio = @total_idle_time / total_report_cycle_time
+
+          reset_idle_report_cycle! if reset
+
+          idle_ratio
+        end
       end
 
       def track_current_state
@@ -64,6 +106,18 @@ module Judoscale
           store.push :pu, active_processes, time # pu = process utilization
           store.push :ru, active_requests, time  # ru = request utilization
         end
+      end
+
+      private
+
+      def current_time
+        Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      end
+
+      def reset_idle_report_cycle!
+        @total_idle_time = 0.0
+        @report_cycle_started_at = current_time
+        @idle_started_at = @report_cycle_started_at if @active_request_counter.value == 0
       end
     end
   end
--- judoscale-rails/test/utilization_middleware_test.rb
+++ judoscale-rails/test/utilization_middleware_test.rb
@@ -122,5 +122,126 @@ module Judoscale
       _(metrics.map(&:identifier)).must_equal %i[pu ru]
       _(metrics.map(&:value)).must_equal [1, 2]
     end
+
+    it "tracks idle ratio based on time spent with no active requests" do
+      # T=0:   Start tracker
+      # T=1:   Request 1 starts -> total_idle_time=1
+      # T=2:   Request 1 ends   -> total_idle_time=1
+      # T=4:   Request 2 starts -> total_idle_time=3 (1 + 2)
+      # T=5:   Request 3 starts -> total_idle_time=3
+      # T=6:   Request 2 ends   -> total_idle_time=3
+      # T=8:   Request 3 ends   -> total_idle_time=3
+      # T=10:  Report cycle     -> total_idle_time=5 (3 + 2), idle_ratio=5/10=0.5
+
+      current_time = 0
+
+      # Stub Process.clock_gettime to return our controlled monotonic time
+      Process.stub(:clock_gettime, ->(_) { current_time }) do
+        # T=0: Tracker starts
+        tracker.start!
+        _(tracker.idle_ratio(reset: false)).must_equal 0.0 # No time has passed yet
+
+        # T=1: Request 1 starts
+        current_time = 1
+        tracker.incr
+        _(tracker.idle_ratio(reset: false)).must_equal 1.0 # 1 second idle out of 1 total second = 100% idle
+
+        # T=2: Request 1 ends
+        current_time = 2
+        tracker.decr
+        _(tracker.idle_ratio(reset: false)).must_equal 0.5 # 1 second idle out of 2 total seconds = 50% idle
+
+        # T=4: Request 2 starts
+        current_time = 4
+        tracker.incr
+        _(tracker.idle_ratio(reset: false)).must_equal 0.75 # 3 seconds idle out of 4 total seconds = 75% idle
+
+        # T=5: Request 3 starts
+        current_time = 5
+        tracker.incr
+        _(tracker.idle_ratio(reset: false)).must_equal 0.6 # 3 seconds idle out of 5 total seconds = 60% idle
+
+        # T=6: Request 2 ends
+        current_time = 6
+        tracker.decr
+        _(tracker.idle_ratio(reset: false)).must_equal 0.5 # 3 seconds idle out of 6 total seconds = 50% idle
+
+        # T=8: Request 3 ends
+        current_time = 8
+        tracker.decr
+        _(tracker.idle_ratio(reset: false)).must_equal 0.375 # 3 seconds idle out of 8 total seconds = 37.5% idle
+
+        # T=10: Report cycle - should calculate final idle ratio
+        current_time = 10
+        _(tracker.idle_ratio).must_equal 0.5 # 5 seconds idle out of 10 total seconds = 50% idle
+      end
+    end
+
+    it "resets the tracking cycle when idle_ratio is requested with no args" do
+      # T=0:   Start tracker
+      # T=1:   Request 1 starts -> total_idle_time=1
+      # T=2:   Request 1 ends   -> total_idle_time=1
+      # T=4:   Report cycle     -> total_idle_time=3 (1 + 2), idle_ratio=3/4=0.75
+      # T=5:   Request 2 starts -> total_idle_time=1
+      # T=8:   Report cycle     -> total_idle_time=1 (request still running), idle_ratio=1/4=0.25
+      # T=9:   Request 3 starts -> total_idle_time=0
+      # T=10:  Request 2 ends   -> total_idle_time=0
+      # T=11:  Request 3 ends   -> total_idle_time=0
+      # T=12:  Report cycle     -> total_idle_time=1, idle_ratio=1/4=0.25
+
+      current_time = 0
+
+      # Stub Process.clock_gettime to return our controlled monotonic time
+      Process.stub(:clock_gettime, ->(_) { current_time }) do
+        # T=0: Tracker starts
+        tracker.start!
+        _(tracker.idle_ratio(reset: false)).must_equal 0.0 # No time has passed yet
+
+        # T=1: Request 1 starts
+        current_time = 1
+        tracker.incr
+        _(tracker.idle_ratio(reset: false)).must_equal 1.0 # 1 second idle out of 1 total second = 100% idle
+
+        # T=2: Request 1 ends
+        current_time = 2
+        tracker.decr
+        _(tracker.idle_ratio(reset: false)).must_equal 0.5 # 1 second idle out of 2 total seconds = 50% idle
+
+        current_time = 3
+        _(tracker.idle_ratio(reset: false)).must_be_close_to 0.6666, 0.001 # 2 seconds idle out of 3 total seconds = 66.66% idle
+
+        # T=4: Report cycle
+        current_time = 4
+        _(tracker.idle_ratio).must_equal 0.75 # 3 seconds idle out of 4 total seconds = 75% idle
+
+        # T=5: Request 2 starts
+        current_time = 5
+        tracker.incr
+        _(tracker.idle_ratio(reset: false)).must_equal 1.0 # 1 second idle out of 1 total second = 100% idle
+
+        # T=8: Report cycle
+        current_time = 8
+        _(tracker.idle_ratio).must_equal 0.25 # 1 second idle out of 4 total seconds = 25% idle
+
+        # T=9: Request 3 starts
+        current_time = 9
+        tracker.incr
+        _(tracker.idle_ratio(reset: false)).must_equal 0.0 # 0 seconds idle out of 1 total second = 0% idle
+
+        # T=10: Request 2 ends
+        current_time = 10
+        tracker.decr
+        _(tracker.idle_ratio(reset: false)).must_equal 0.0 # 0 seconds idle out of 2 total second = 0% idle
+
+        # T=11: Request 3 ends
+        current_time = 11
+        tracker.decr
+        _(tracker.idle_ratio(reset: false)).must_equal 0.0 # 0 seconds idle out of 3 total second = 0% idle
+
+        # T=12: Report cycle
+        current_time = 12
+        _(tracker.idle_ratio).must_equal 0.25 # 1 second idle out of 4 total seconds = 25% idle
+      end
+    end
   end
 end
