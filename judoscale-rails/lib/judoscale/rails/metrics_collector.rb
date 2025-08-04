3ea615e682684100cd6ce93084e7265baaf30d06
Adam McCrea <adam@adamlogic.com>
2025-08-04 09:14:24 -0400

    Move utilization collection to Rails-specific collector

---
+++ judoscale-rails/lib/judoscale/rails/metrics_collector.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "judoscale/web_metrics_collector"
+require "judoscale/metric"
+require "judoscale/config"
+require "judoscale/rails/utilization_middleware"
+
+module Judoscale
+  module Rails
+    class MetricsCollector < Judoscale::WebMetricsCollector
+      def collect
+        metrics = super
+
+        if Judoscale::Config.instance.utilization_enabled
+          # Report utilization percentage as a whole number (floats not supported)
+          idle_ratio = UtilizationTracker.instance.idle_ratio
+          utilization = (1.0 - idle_ratio) * 100.0
+          metrics.push(Metric.new(:up, utilization, Time.now)) # Utilization percentage
+        end
+
+        metrics
+      end
+    end
+  end
+end
--- judoscale-ruby/lib/judoscale/web_metrics_collector.rb
+++ judoscale-ruby/lib/judoscale/web_metrics_collector.rb
@@ -2,21 +2,11 @@
 
 require "judoscale/metrics_collector"
 require "judoscale/metrics_store"
-require "judoscale/rails/utilization_middleware"
 
 module Judoscale
   class WebMetricsCollector < MetricsCollector
     def collect
-      metrics = MetricsStore.instance.flush
-      # TODO: Move this to a new Judoscale::Rails::MetricsCollector class
-      if Judoscale::Config.instance.utilization_enabled
-        # Report utilization percentage as a whole number (floats not supported)
-        idle_ratio = Judoscale::Rails::UtilizationTracker.instance.idle_ratio
-        utilization = (1.0 - idle_ratio) * 100.0
-        metrics.push(Metric.new(:up, utilization, Time.now)) # Utilization percentage
-      end
-
-      metrics
+      MetricsStore.instance.flush
     end
   end
 end
