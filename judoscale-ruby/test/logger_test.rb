702bc1e3ccabc6a3c8db08170fec97bf5f466631
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2025-12-22 11:53:16 -0300

    Handle loggers with `level` returning an identifier instead of integer
    Ruby's Logger uses Integer values to represent each "level" / severity,
    and most other logging libraries follow that API.
    
        > ::Logger::DEBUG
        => 0
        > ::Logger::INFO
        => 1
    
    https://github.com/ruby/logger/blob/e6bf1da59e8c3453c480d6bab9492eb797ccf455/lib/logger/severity.rb#L6-L17
    
    However, Semantic Logger (and maybe some other libraries we don't know
    about) uses levels with identifiers such as `:debug` directly
    
        > SemanticLogger::Levels::LEVELS
        => [:trace, :debug, :info, :warn, :error, :fatal]
    
    https://github.com/reidmorrison/semantic_logger/blob/bae745b222e99170487575d491e4592c08cc856f/lib/semantic_logger/base.rb#L12-L35
    https://github.com/reidmorrison/semantic_logger/blob/bae745b222e99170487575d491e4592c08cc856f/lib/semantic_logger/levels.rb#L4
    
    which we need to map to logger "integer" levels, othewise we get errors
    trying to compare them:
    
        lib/judoscale/logger.rb:25:in 'Integer#>=':
        comparison of Integer with :debug failed (ArgumentError)
    
                  if severity_level >= logger.level
    
    In order to handle these, we will try to coerce the underlying logger
    level to integer so we can compare both the log severity level (from
    Ruby's ::Logger) to the underlying logger level correctly.
    
    This exception would happen inside our Reporter thread, which could kill
    the thread and stop reporting metrics altogether.

--- judoscale-ruby/test/logger_test.rb
+++ judoscale-ruby/test/logger_test.rb
@@ -73,5 +73,40 @@ module Judoscale
         end
       end
     end
+
+    class LoggerSymbol < ::Logger
+      def level
+        ::Logger::SEV_LABEL[super].downcase.to_sym
+      end
+
+      def level=(new_level)
+        super ::Logger::Severity.coerce(new_level)
+      end
+
+      def add(severity, message = nil, *)
+        # Bypass the severity/level check inside Logger that'd fail with Integer vs symbol too.
+        # Log everything.
+        @logdev.write("LEVEL=#{level} SEVERITY=#{severity} #{message || yield}")
+        true
+      end
+    end
+
+    it "gracefully handles logger level using symbols/strings (such as rails-semantic-logger)" do
+      original_logger = LoggerSymbol.new(string_io)
+      original_logger.level = :info
+
+      use_config logger: original_logger, log_level: :warn do
+        logger.info "some info"
+        _(messages).wont_include "some info"
+
+        logger.warn "some warning"
+        _(messages).must_include "LEVEL=info SEVERITY=2 [Judoscale] some warning"
+
+        original_logger.level = :ERROR
+        logger.warn "other warning"
+
+        _(messages).must_include "LEVEL=error SEVERITY=3 [Judoscale] [WARN] other warning"
+      end
+    end
   end
 end
